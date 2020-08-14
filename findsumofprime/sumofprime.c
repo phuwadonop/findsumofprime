3f37417da1d513a7d58453f3af1c917b4c143759
phu wadon <pmmp_1599@hotmail.com>
2020-08-14 13:25:36 +0700

    debug

--- findsumofprime/sumofprime.c
+++ findsumofprime/sumofprime.c
@@ -9,10 +9,12 @@ int main() {
     for (long int i = 0;i < max; i++) {
         a[i] = i + 1;
     }
-    for (long int k = 1;k < sqrt(max);k++) {
+    long long int  sum = 0;
+    for (long int k = 1;k < max;k++) {
         if (a[k] != 0) {
             n = a[k];
             multiply = n;
+            sum += a[k];
             for (unsigned long int j = 2; multiply < max; j++) {
                 multiply = n * j;
                 a[multiply - 1] = 0;
@@ -20,11 +22,5 @@ int main() {
 
         }
     }
-    long long int  sum = 0;
-    for (unsigned long long int m = 1;m < max - 1;m++) {
-        if (a[m] != 0) { sum += a[m]; }
-    }
     printf("%lld", sum);
 }
-
-
