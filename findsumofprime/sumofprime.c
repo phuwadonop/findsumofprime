cd4932ef4d386b32fb87c570a67bf3a116023f98
phu wadon <pmmp_1599@hotmail.com>
2020-08-14 13:13:52 +0700

    add math.h

--- findsumofprime/sumofprime.c
+++ findsumofprime/sumofprime.c
@@ -1,38 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include<stdio.h>
-
+#include<math.h>
 #define N 4000000
-
 int main() {
 
     long int max, n, multiply;
 
     static long int a[N];
 
-    long long int  sum = 0;
-
     scanf("%lld", &max);
 
     for (long int i = 0;i < max; i++) {
         a[i] = i + 1;
     }
-    for (long int k = 1;k < max;k++) {
+    for (long int k = 1;k < sqrt(max);k++) {
         if (a[k] != 0) {
-            sum += a[k];
             n = a[k];
             multiply = n;
-            for (long int j = 2; multiply < max; j++) {
+            for (unsigned long int j = 2; multiply < max; j++) {
                 multiply = n * j;
                 a[multiply - 1] = 0;
             }
 
         }
     }
-    //long long int  sum = 0;
-    /*for (long long int m = 1;m < max - 1 ;m++) {
+    long long int  sum = 0;
+    for (unsigned long long int m = 1;m < max - 1;m++) {
         if (a[m] != 0) { sum += a[m]; }
-    }*/
+    }
     printf("%lld", sum);
-
 }
+
+
